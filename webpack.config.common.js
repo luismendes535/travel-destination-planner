2380595b87976dd1790b28ac81efecd923e10b77
Luis Mendes <luismendes535@gmail.com>
2023-08-14 18:14:09 +0100

    added html-webpack-plugin plugin to copy html file to dist folder

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,28 +1,30 @@
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
   },
   devServer: {
     hot: true,
-    static: path.resolve(__dirname, 'dist'),
+    static: path.resolve(__dirname, "dist"),
   },
   plugins: [
     new CopyWebpackPlugin({
-      patterns: [
-        { from: 'src/assets', to: 'assets' },
-      ],
+      patterns: [{ from: "src/assets", to: "assets" }],
+    }),
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
     }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
